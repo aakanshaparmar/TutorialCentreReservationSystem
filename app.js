42dd350643fa9bb5587238996a8a827560cd05ec
EC2 Default User <ec2-user@ip-172-31-29-193.ap-southeast-1.compute.internal>
2016-04-01 13:09:52 +0000

    Few changes in app.js

--- app.js
+++ app.js
@@ -11,6 +11,31 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/signUp.html'));
     });
    
+app.get('/studentSignIn.html',function(req,res){
+    res.sendFile(path.join(__dirname+'/studentSignIn.html'));
+    });
+   
+app.get('/tutorSignIn.html',function(req,res){
+    res.sendFile(path.join(__dirname+'/tutorSignIn.html'));
+    });
+   
+app.get('/endMessage.html',function(req,res){
+    res.sendFile(path.join(__dirname+'/endMessage.html'));
+    });
+   
+app.get('/selectTimes.html',function(req,res){
+    res.sendFile(path.join(__dirname+'/selectTimes.html'));
+    });
+   
+app.get('/profile.html',function(req,res){
+    res.sendFile(path.join(__dirname+'/profile.html'));
+    });
+
+app.get('/studentProfile.html',function(req,res){
+    res.sendFile(path.join(__dirname+'/studentProfile.html'));
+    });
+   
+
       app.listen(80);
 
     
